9a49364d379ab104411e039d65a3e4fadeb33951
Ilhom Musayev <ilhommusayev2001@gmail.com>
2023-04-01 07:29:00 +0500

    tomom

--- src/hooks/useToken.jsx
+++ src/hooks/useToken.jsx
@@ -1,10 +1,9 @@
-import { useContext } from 'react';
-import { Context } from '../contexts/AuthContext';
+import { useContext } from "react";
+import { Context } from "../contexts/AuthContext";
 
 const useToken = (setterOnly) => {
   const ctx = useContext(Context);
 
   return setterOnly ? [ctx.setState] : [ctx.state, ctx.setState];
 };
-
 export default useToken;
